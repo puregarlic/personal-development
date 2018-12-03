ea0ec18384ae6c53573baf79160418b82065af09
Graham Barber <grahammbarber@gmail.com>
2018-12-02 22:56:56 -0800

    fix(content): final editing pass

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ class Website extends Component {
               <SubSectionGroup pose={one ? 'enter' : ''}>
                 <PosedSubSection title="Raised with self-awareness" initialPose="exit">
                   Raised by a civil engineer and a librarian, I've been trained from an early age to
-                  seek my own answers and constantly evaluate how I appear to others, how my actions
-                  will affect my future, and how my actions might affect those around me.{' '}
+                  seek my own answers and to constantly evaluate how I appear to others, how my
+                  actions will affect my future, and how my actions might affect those around me.{' '}
                   <Highlight>
                     It's never good enough for me to simply take an action — I need to be
                     consciously aware of the reasoning behind it.
@@ -73,7 +73,8 @@ class Website extends Component {
                   In high school, I was well known for coming up with bold, creative ideas for video
                   games and software. Early on, I swore that I would never learn to program, because
                   there would always be someone else who would be willing and capable of doing so
-                  for me. By the end of high school, I wisened up, and figured out that{' '}
+                  for me. By the end of high school and many failed projects, I wisened up, and
+                  figured out that{' '}
                   <Highlight>
                     if anybody is going to bring my ideas to life, it's going to have to be myself.
                   </Highlight>
@@ -88,7 +89,8 @@ class Website extends Component {
                   I'm adept at taking an obstacle and breaking it down into smaller, solvable
                   portions — a practice called computational thinking. Whether it's a scheduling
                   issue, a conflict of interest, or a difficult class assignment, I will find a
-                  solution. <Highlight>There's no such thing as an impossible challenge.</Highlight>
+                  solution. <Highlight>There's no such thing as an impossible challenge,</Highlight>{' '}
+                  especially for a team.
                 </PosedSubSection>
                 <PosedSubSection title="Technical expertise" initialPose="exit">
                   Whether it's IT troubleshooting, programming, graphic design, or merely finding
@@ -99,7 +101,7 @@ class Website extends Component {
                 <PosedSubSection title="Public speaking" initialPose="exit">
                   After years of sharing my ideas with friends and family, I put the time in to
                   learn how to present them in a professional format. I've developed a skill for
-                  rephrasing ideas into language that a non-expert can undestand, and{' '}
+                  rephrasing ideas into language that non-experts can understand, and{' '}
                   <Highlight>I've grown comfortable presenting to an audience.</Highlight>
                 </PosedSubSection>
               </SubSectionGroup>
@@ -131,18 +133,18 @@ class Website extends Component {
                   I check in with each of my team members to ensure that they feel like they're
                   adding to the project's value. If they don't feel that way, then our team is not
                   reaching its maximum potential, so I either find work that they find is more
-                  valuable, or I help them find value in their work.
+                  valuable, or I help them find the value in their work.
                 </PosedSubSection>
                 <PosedSubSection title="Be a little skeptical" initialPose="exit">
-                  "Healthy skepticism" is truly a useful tool in a diverse team.{' '}
+                  "Healthy skepticism" is a useful tool in a diverse team.{' '}
                   <Highlight>
                     Being skeptical of a commonly-accepted idea among the group provides an
-                    opportunity for other, possibly trampled ideas to come to the surface,
+                    opportunity for other, possibly trampled-upon ideas to come to the surface,
                   </Highlight>{' '}
                   contributing to the group's discussion and perspectives — also ensuring that your
                   team understands why they're doing what they're doing. So, I play devil's advocate
-                  when I believe there are alternatives to our selected approach, or I believe there
-                  are underrepresented ideas within the team.
+                  when there are alternatives to our selected approach, or I believe there are
+                  underrepresented ideas within the team.
                 </PosedSubSection>
               </SubSectionGroup>
             </ScrollTrigger>
